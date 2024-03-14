9f65861ad1a20b4fa7c63d7bf4200cab58fdf6ef
AUVADAIAPPAN S <130080437+Auvadaiappan@users.noreply.github.com>
2024-03-14 19:40:28 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:avd_basics/quiz.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Quiz());
+}
---
+++ lib/quiz.dart
@@ -0,0 +1,82 @@
+import 'package:avd_basics/data/questions.dart';
+import 'package:avd_basics/questions_screen.dart';
+import 'package:avd_basics/results_screen.dart';
+import 'package:avd_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'question-screen';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 232, 188, 185),
+                Color.fromARGB(255, 243, 159, 90),
+                Color.fromARGB(255, 174, 68, 90),
+                Color.fromARGB(255, 102, 37, 73),
+                Color.fromARGB(255, 69, 25, 82),
+                Color.fromARGB(255, 29, 28, 55),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
